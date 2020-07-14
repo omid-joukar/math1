122030c9721c4dce6d443a274f389c35102d610c
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-14 10:02:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by KPS on 7/14/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+            int tot_theory_marks = 350, tot_practical_marks = 90, tot_marks = 500;
+            int percentage_of_marks = ((tot_theory_marks + tot_practical_marks)*100)/tot_marks;
+            System.out.print("\nPercentage of Marks: "+percentage_of_marks+"%\n");
+        }
+    }
+
